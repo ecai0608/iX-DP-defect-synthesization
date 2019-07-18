de583da673f621eb2c8de7baca22d17d97941300
Eric Cai <ecai@wustl.edu>
2019-07-18 08:40:10 +0200

    generate defect (red dot) at random vertex

--- blemishOBJ.py
+++ blemishOBJ.py
@@ -1,4 +1,5 @@
 import bpy
+from bpy_extras.object_utils import world_to_camera_view
 import numpy as np 
 import random
 
@@ -7,47 +8,49 @@ import random
 def blemishOBJ(obj):
     #obj = bpy.data.objects[object_name]
 
-    # checking if the "blemish" material has already been added to the object
-    if "blemish" not in obj.data.materials :
-        # making the material and setting it to a red color
-        blemish_mat = bpy.data.materials.new("blemish")
-        blemish_mat.diffuse_color = (1, 0, 0)
+    # choosing a random vertex
+    rand_vert = random.choice(obj.data.vertices)
+    co_3d = rand_vert.co
 
-        # checking if there are no material slots - if there are not, a dummy material slot needs to be added first
-        if len(obj.data.materials) <= 1 :
-            obj.data.materials.append(bpy.data.materials.new("material"))
+    # creating and placing marker at defect vertex
+    bpy.ops.mesh.primitive_uv_sphere_add()
+    marker = bpy.data.objects["Sphere"]
+    marker.location = co_3d
+    # scaling marker to smaller size
+    marker.scale.x = 0.01
+    marker.scale.y = 0.01
+    marker.scale.z = 0.01
 
-        # adding material to object
-        obj.data.materials.append(blemish_mat)
+    # adding red material to marker for visibility
+    blemish_mat = bpy.data.materials.new("blemish")
+    blemish_mat.diffuse_color = (1, 0, 0)
+    marker.data.materials.append(blemish_mat)
+
+    # setting scene/resolution variables for clarity
+    scene = bpy.context.scene
+    render = scene.render
+    res_x = render.resolution_x
+    res_y = render.resolution_y
     
-    # obtaining index of blemish material
-    blemish_index = list(obj.data.materials).index(bpy.data.materials["blemish"])
-    
-    
-    # choosing random face on mesh and assigning it blemish material
-    rand_face = random.choice(obj.data.polygons)
-    rand_face.material_index = blemish_index
+    # iterating through all cameras to render images
+    for obj in bpy.data.objects :
+        if (obj.type == "CAMERA") :
+            # setting camera to active camera
+            bpy.context.scene.camera = obj
 
-    
-    # rendering all 4 images
-    bpy.context.scene.camera = bpy.data.objects["front_cam"]
-    bpy.ops.render.render(write_still = True)
-    bpy.data.images['Render Result'].save_render(filepath = "renders/front_render.png")
+            # computing image coordinates
+            co_2d = world_to_camera_view(scene, obj, co_3d)
+            x, y = round(res_x*co_2d[0]), round(res_y*co_2d[1])
 
-    bpy.context.scene.camera = bpy.data.objects["back_cam"]
-    bpy.ops.render.render(write_still = True)
-    bpy.data.images['Render Result'].save_render(filepath = "renders/back_render.png")
-
-    bpy.context.scene.camera = bpy.data.objects["top_cam"]
-    bpy.ops.render.render(write_still = True)
-    bpy.data.images['Render Result'].save_render(filepath = "renders/top_render.png")
-
-    bpy.context.scene.camera = bpy.data.objects["bot_cam"]
-    bpy.ops.render.render(write_still = True)
-    bpy.data.images['Render Result'].save_render(filepath = "renders/bot_render.png")
+            # saving image render to "renders" folder
+            bpy.ops.render.render(write_still = True)
+            bpy.data.images["Render Result"].save_render(filepath = ("renders/%s.png" % obj.name))
+            # writing image coordinates to text file
+            binfile = open(("renders/%s.txt" % obj.name), "w")
+            binfile.write("Image Coordinates: %s,%s" % (x, y))
 
 
-
+# running on test object
 test_object = bpy.data.objects["test_object"]
 blemishOBJ(test_object)
 
